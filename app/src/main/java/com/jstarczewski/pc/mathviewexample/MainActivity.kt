a29fa28879bd0a67a7aa7215cb2e28268466bbfe
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-22 13:47:20 +0100

    Not working displaying math equations

--- app/src/main/java/com/jstarczewski/pc/mathviewexample/MainActivity.kt
+++ app/src/main/java/com/jstarczewski/pc/mathviewexample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.jstarczewski.pc.mathviewexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jstarczewski.pc.mathview.src.MathView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mathView: MathView = findViewById<MathView>(R.id.mvTest) as MathView
+
+        mathView.setText("'$\\x(t)=Asin(ωt+ϕ_o)[m]$, $\\x_{max}=A$ <br> $\\x(t)$ położenie od czasu, $\\A$ amplituda, $\\ω$ częstość kołowa, $\\t$ czas, $\\ϕ_o$ przesunięcie początkowe'")
+
+
     }
 }
---
+++ mathview/src/main/java/com/jstarczewski/pc/mathview/src/MathView.kt
@@ -0,0 +1,59 @@
+package com.jstarczewski.pc.mathview.src
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+
+/*
+class MathView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : WebView(context, attrs, defStyleAttr) {
+
+    fun foo(testValue: Int) : Int {
+       return testValue*2
+    }
+
+}*/
+
+class MathView : WebView {
+
+    private lateinit var mText: String
+    private val path: String = "file:///android_asset/"
+    private val baseStart: String = "<html><head>" +
+            "<link rel='stylesheet' href='" + path + "jqmath-0.4.3.css'>" +
+            "<script src='" + path + "jquery-1.4.3.min.js'></script>" +
+            "<script src='" + path + "jqmath-etc-0.4.5.min.js'></script>" +
+            "</head><body>" +
+            "<script>var s = "
+    private val baseEnding: String = ";M.parseMath(s);document.body.style.fontSize = \"13pt\";document.body.style.textAlign = \"center\";document.write(s);</script></body>"
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        settings.javaScriptEnabled = true
+        settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        Log.e("Init", "initialized")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            setLayerType(View.LAYER_TYPE_HARDWARE, null)
+        else
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+    }
+    fun setText(text: String) {
+
+        this.settings.javaScriptEnabled = true
+        this.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        mText = text
+        Log.e("Text", baseStart+mText+baseEnding)
+        this.loadDataWithBaseURL(path, baseStart + mText + baseEnding, "text/html", "UTF-8", null)
+
+/*
+        webViews[i].loadDataWithBaseURL("file:///android_asset/", equations.get(i), "text/html", "UTF-8", null);
+        */
+    }
+
+}
