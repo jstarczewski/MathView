5c1e0b12dcdcf3746cc033b9cdad979b06d6aee1
jstarczewski <jan.a.starczewski@gmail.com>
2018-11-23 14:43:45 +0100

    Recycler view created

--- mathview/src/main/java/com/jstarczewski/pc/mathview/src/MathView.kt
+++ mathview/src/main/java/com/jstarczewski/pc/mathview/src/MathView.kt
@@ -1,23 +1,17 @@
 package com.jstarczewski.pc.mathview.src
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.ProgressBar
 
 class MathView : WebView {
 
     private val path: String = "file:///android_asset/"
     private var mText = "Your equation"
     private var mBase: String = "<html><head><link rel='stylesheet' href='" + path + "jqmath-0.4.3.css'><script src='" + path + "jquery-1.4.3.min.js'></script><script src='" + path + "jqmath-etc-0.4.5.min.js'></script></head><body><script>var s = $mText;M.parseMath(s);document.body.style.fontSize = \"13pt\";document.body.style.textAlign = \"center\";document.write(s);</script></body>";
-    private lateinit var progressBar: ProgressBar
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -31,9 +25,6 @@ class MathView : WebView {
         else
             setLayerType(View.LAYER_TYPE_SOFTWARE, null)
 
-
-
-
     }
 
     fun setText(text: String) {
@@ -43,16 +34,7 @@ class MathView : WebView {
 
     }
 
-    private fun setUpProgressBar() {
-
-        progressBar = 
-
-    }
-
     private fun updateBase() {
-
-
-
         mBase = "<html><head><link rel='stylesheet' href='" + path + "jqmath-0.4.3.css'><script src='" + path + "jquery-1.4.3.min.js'></script><script src='" + path + "jqmath-etc-0.4.5.min.js'></script></head><body><script>var s = $mText;M.parseMath(s);document.body.style.fontSize = \"13pt\";document.body.style.textAlign = \"center\";document.write(s);</script></body>"
     }
 }
